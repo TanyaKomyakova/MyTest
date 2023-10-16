27b11e46c7c2eabfaf7327f8c055eb4ddecadcbc
TanyaKomyakova <tanya_komyakova@mail.ru>
2023-10-16 17:27:53 +0300

    task about Test

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,2 +1,11 @@
+import java.sql.SQLOutput;
+
 public class MyTest {
+    public static void main(String[] args){
+        int price1 = 2000;
+        int price2 = 500;
+        int sum = price1 + price2;
+        System.out.println(sum);
+
+    }
 }
