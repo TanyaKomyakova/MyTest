853ae7c99e5e633bf09192400ffb318cb726f1d1
TanyaKomyakova <tanya_komyakova@mail.ru>
2023-10-24 16:40:56 +0300

    first MoneyTransfer

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int price1 = 2000;
+        int price2 = 500;
+        int sum = price1 + price2;
+        System.out.println(sum);
+    }
+}
